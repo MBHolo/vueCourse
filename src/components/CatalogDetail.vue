40d608bfed2eb7a2a2836e1e80891b29de5318ce
MBHolo <maximb7474@gmail.com>
2025-08-24 19:56:27 +0500

    second commit

---
+++ src/components/CatalogDetail.vue
@@ -0,0 +1,58 @@
+<template>
+    <div v-if="!itemChoose"class="item__choose-enemy">Choose item</div>
+    <div v-else class="item__choose-choose" :id="itemChoose.id">
+        <div class="item__choose-left__block">
+            <div class="item__choose-image">
+                <img :src="itemChoose.image" alt="">
+            </div>
+        </div>
+        <div class="item__choose-right__block">
+            <div class="item__choose-main__info">
+                <div class="main__info-rating__stars">
+                    <div class="star"></div>
+                </div>
+                <h1 class="main__info-title">{{ itemChoose.title }}</h1>
+                <div class="main__info-price">Price: {{ itemChoose.price }}</div>
+                <div class="main__info-description">{{ itemChoose.description }}</div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+    defineProps(["itemChoose"]);
+</script>
+
+<style scoped>
+    .item__choose-enemy{
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        height: 100%;
+        align-items: center;
+        font-size: 40px;
+        font-weight: 700;
+    }
+    .item__choose-choose{
+        display: flex;
+        height: 100%;
+        width: 100%;
+        padding: 20px;
+    }
+    .item__choose-left__block{
+        width: 50%;
+        padding: 20px;
+        border-radius: 4px;
+        border: 1px solid #ffffff0a;
+    }
+    .item__choose-right__block{
+        width: 50%;
+        padding: 20px;
+    }  
+    .main__info-title{
+        font-size: 20px 
+    }
+    .main__info-description{
+        margin-top: 20px;
+    }
+</style>
---
+++ src/components/CatalogList.vue
@@ -0,0 +1,74 @@
+<template>
+    <a :href="'#' + item.id" class="item" @click="chooseItem">
+        <div class="item-image">
+            <img :src="item.image" alt="">
+        </div>
+        <div class="item-main__info">
+            <div class="item-title">
+                <div href="">{{ item.title }}</div>
+            </div>
+            <div class="item-price">
+                <span>{{ item.price }}</span>
+            </div>
+        </div>
+    </a>
+</template>
+
+<script setup>
+    defineProps(["item"]);
+
+    const emit = defineEmits();
+
+    function chooseItem(){
+        emit("choose");
+
+    }
+</script>
+
+<style scoped>
+    a{
+        color: #fff;
+    }
+    .item{
+        padding: 10px;
+        border: 1px solid #ffffff0a;
+        border-radius: 4px;
+        background-color: #ffffff00;
+        transition: background-color .5s;
+        height: 400px;
+        display: flex;
+        flex-direction: column;
+        cursor: pointer;
+    }
+
+    .item:hover{
+        background-color: #ffffff0a;
+    }
+
+    .item-title div{
+        color: #fff;
+    }
+
+    .item-image{
+        height: 250px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex: 0 1 250px
+    }
+
+    .item-image img{
+        width: auto;
+        max-width: 50%;
+        max-height: 60%;
+        height: auto;
+        display: block;
+    }
+
+    .item-main__info{
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        flex: 1 1 auto
+    }
+</style>
